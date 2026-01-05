dca9aa24aa18d05a996c97a3b0187b44dc968a07
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-05 21:11:49 +0000

    Added toggleable faint component lines

--- VectorVisualiser.cs
+++ VectorVisualiser.cs
@@ -7,6 +7,7 @@ public class VectorVisualiser : VisualiserBase
     [Header("Options")]
     public bool normalised;
     public bool multiplyByT;
+    public bool showAxisLines;
     public Color displayColour = Color.red;
 
 
@@ -23,8 +24,30 @@ public class VectorVisualiser : VisualiserBase
             displayVector *= t;
         }
 
+        // Draw the main vector
         Gizmos.color = displayColour;
         Gizmos.DrawLine(Vector3.zero, displayVector);
         Gizmos.DrawSphere(displayVector, 0.1f);
+
+
+        // Draw faint axis lines if enabled
+        if (showAxisLines)
+        {
+            // Make a faint version of the display colour
+            Color faint = displayColour;
+            faint.a = 0.2f; // Opacity
+            Gizmos.color = faint;
+
+            // X component line 
+            Gizmos.DrawLine(new Vector3(displayVector.x, 0f, 0f), displayVector);
+
+            // Y component line 
+            Gizmos.DrawLine(new Vector3(0f, displayVector.y, 0f), displayVector);
+
+            // Z component line 
+            Gizmos.DrawLine(new Vector3(0f, 0f, displayVector.z), displayVector);
+        }
     }
+
+
 }
