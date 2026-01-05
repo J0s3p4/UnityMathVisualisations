50532912926c660d69b32287d34fe6b5f7a007f7
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-05 20:30:08 +0000

    Added Options to Vector Visualiser

--- VectorVisualiser.cs
+++ VectorVisualiser.cs
@@ -4,10 +4,27 @@ public class VectorVisualiser : VisualiserBase
 {
     public Vector3 vector = new Vector3(2, 1, 0);
 
+    [Header("Options")]
+    public bool normalised;
+    public bool multiplyByT;
+    public Color displayColour = Color.red;
+
+
     protected override void Draw()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(Vector3.zero, vector);
-        Gizmos.DrawSphere(vector, 0.1f);
+        Vector3 displayVector = vector; // v -> f(v)
+
+        if (normalised)
+        {
+            displayVector = displayVector.normalized;
+        }
+        if (multiplyByT)
+        {
+            displayVector *= t;
+        }
+
+        Gizmos.color = displayColour;
+        Gizmos.DrawLine(Vector3.zero, displayVector);
+        Gizmos.DrawSphere(displayVector, 0.1f);
     }
 }
