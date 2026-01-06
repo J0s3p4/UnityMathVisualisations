38c96a3efc1b3f20b4130fa5997e5a294b656c30
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-06 14:04:07 +0000

    Can now show negative B on A in the subtraction settings

--- Vectors/VectorSubtractionVisualiser.cs
+++ Vectors/VectorSubtractionVisualiser.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class VectorSubtractionVisualiser : VectorOperationVisualiserBase
 {
+    [Header("Subtraction Specifics")]
+    public bool showNegativeBOnA;
 
     protected override Vector3 PerformOperation(Vector3 a, Vector3 b) => a - b;
 
     protected override string GetOperationName() => "A - B";
 
+
+    protected override void DrawExtraVisuals(Vector3 a, Vector3 b, Vector3 result)
+    {
+
+        if (showNegativeBOnA)
+        {
+            Gizmos.color = vectorB.displayColour;
+            Gizmos.DrawLine(a, a - b);
+            Gizmos.DrawSphere(a - b, resultSphereRadius * 0.5f);
+        }
+    }
 
 }
