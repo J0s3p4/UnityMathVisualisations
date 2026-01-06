2d353c0a93e02b98f91d3389f011d9ecfa411c12
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-06 19:55:44 +0000

    Can now switch between subtracting B from A and A from B

--- Vectors/VectorSubtractionVisualiser.cs
+++ Vectors/VectorSubtractionVisualiser.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 public class VectorSubtractionVisualiser : VectorOperationVisualiserBase
 {
     [Header("Subtraction Settings")]
-    public bool showNegativeBOnA;
+    public bool showSubtractingVectorLine;
+    public bool subtractBByA = false;
 
-    protected override Vector3 PerformOperation(Vector3 a, Vector3 b) => a - b;
+    protected override Vector3 PerformOperation(Vector3 a, Vector3 b) => subtractBByA ? b - a : a - b;
 
-    protected override string GetOperationName() => "A - B";
+  
+    protected override string GetOperationName() => subtractBByA ? "B - A" : "A - B";
 
 
     protected override void DrawExtraVisuals(Vector3 a, Vector3 b, Vector3 result)
     {
 
-        if (showNegativeBOnA)
+        if (showSubtractingVectorLine)
         {
-            Gizmos.color = vectorB.displayColour;
-            UnityEditor.Handles.color = vectorB.displayColour;
-            UnityEditor.Handles.DrawDottedLine(a, a - b, 5f);
-            Gizmos.DrawSphere(a - b, resultSphereRadius * 0.5f);
+
+            if (subtractBByA)
+            {
+                Gizmos.color = vectorA.displayColour;
+                UnityEditor.Handles.color = vectorA.displayColour;
+                UnityEditor.Handles.DrawDottedLine(b, b - a, 5f);
+                Gizmos.DrawSphere(b - a, resultSphereRadius * 0.5f);
+
+            }
+            else
+            {
+                Gizmos.color = vectorB.displayColour;
+                UnityEditor.Handles.color = vectorB.displayColour;
+                UnityEditor.Handles.DrawDottedLine(a, a - b, 5f);
+                Gizmos.DrawSphere(a - b, resultSphereRadius * 0.5f);
+            }
+
         }
     }
 
