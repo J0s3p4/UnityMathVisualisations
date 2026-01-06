aac0ffd57cecdc94e14d872280daf7bb3d343d3d
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-06 16:56:40 +0000

    Added Dot product

---
+++ Vectors/VectorDotProductVisualiser.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEngine;
+
+public class VectorDotProductVisualiser : VectorOperationVisualiserBase
+{
+    private float dot;
+
+    protected override Vector3 PerformOperation(Vector3 a, Vector3 b)
+    {
+
+        // Calculate dot product
+        dot = Vector3.Dot(a, b);
+        
+        // Tell the base not to draw the label.
+        showValue = false;
+        // Return zero, no vector
+        return Vector3.zero;
+    }
+
+    protected override string GetOperationName() => "A . B";
+
+    protected override void DrawExtraVisuals(Vector3 a, Vector3 b, Vector3 result)
+    {
+    #if UNITY_EDITOR
+        // Draw dot product between tips of vectorA & vectorB
+        Vector3 midpoint = (a + b) * 0.5f;
+        Handles.Label(
+            midpoint,
+            $"A · B = {dot:F2}"
+        );
+    #endif
+    }
+}
