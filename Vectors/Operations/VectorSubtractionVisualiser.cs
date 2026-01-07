0cc0469e2dd547c03a5f261f1b735ed0f039fdf6
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-07 18:49:43 +0000

    Updated subtraction visualiser to use enum, and updated basic operations scene for changes

--- Vectors/Operations/VectorSubtractionVisualiser.cs
+++ Vectors/Operations/VectorSubtractionVisualiser.cs
@@ -3,14 +3,32 @@ using UnityEngine;
 
 public class VectorSubtractionVisualiser : VectorOperationVisualiserBase
 {
-    [Header("Subtraction Settings")]
-    public bool showSubtractingVectorLine;
-    public bool subtractBByA = false;
 
-    protected override Vector3 PerformOperation(Vector3 a, Vector3 b) => subtractBByA ? b - a : a - b;
+    public enum SubtractionToCalculate
+    {
+        ASubtractB,
+        BSubtractA
+    }
+
+    [Header("Subtraction Settings")]
+    public SubtractionToCalculate subtractiontocalulate;
+    public bool showSubtractingVectorLine;
+
+    protected override Vector3 PerformOperation(Vector3 a, Vector3 b)
+    {
+        return subtractiontocalulate == SubtractionToCalculate.ASubtractB
+            ? a - b
+            : b - a;
+    } 
 
   
-    protected override string GetOperationName() => subtractBByA ? "B - A" : "A - B";
+    protected override string GetOperationName()
+    {
+        return subtractiontocalulate == SubtractionToCalculate.ASubtractB
+            ? "A - B"
+            : "B - A";
+    }
+
 
 
     protected override void DrawExtraVisuals(Vector3 a, Vector3 b, Vector3 result)
@@ -19,20 +37,20 @@ public class VectorSubtractionVisualiser : VectorOperationVisualiserBase
         if (showSubtractingVectorLine)
         {
 
-            if (subtractBByA)
-            {
-                Gizmos.color = vectorA.displayColour;
-                UnityEditor.Handles.color = vectorA.displayColour;
-                UnityEditor.Handles.DrawDottedLine(b, b - a, 5f);
-                Gizmos.DrawSphere(b - a, resultSphereRadius * 0.5f);
-
-            }
-            else
+            if (subtractiontocalulate == SubtractionToCalculate.ASubtractB)
             {
                 Gizmos.color = vectorB.displayColour;
                 UnityEditor.Handles.color = vectorB.displayColour;
                 UnityEditor.Handles.DrawDottedLine(a, a - b, 5f);
                 Gizmos.DrawSphere(a - b, resultSphereRadius * 0.5f);
+
+            }
+            if (subtractiontocalulate == SubtractionToCalculate.BSubtractA)
+            {
+                Gizmos.color = vectorA.displayColour;
+                UnityEditor.Handles.color = vectorA.displayColour;
+                UnityEditor.Handles.DrawDottedLine(b, b - a, 5f);
+                Gizmos.DrawSphere(b - a, resultSphereRadius * 0.5f);
             }
 
         }
