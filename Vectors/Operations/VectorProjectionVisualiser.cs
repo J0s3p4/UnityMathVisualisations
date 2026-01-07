779e1080d6c640c80f057f7ea10292d6a476be8f
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-07 15:37:55 +0000

    Added functional vector projections

--- Vectors/Operations/VectorProjectionVisualiser.cs
+++ Vectors/Operations/VectorProjectionVisualiser.cs
@@ -1,20 +1,43 @@
 using UnityEngine;
+using static VectorProjectionVisualiser;
 
 public class VectorProjectionVisualiser : VectorOperationVisualiserBase
 {
 
 
+  
+    public enum ProjectionMode
+    {
+        drawProjectedAOnB,
+        drawProjectedBOnA
+    }
+    
     [Header("Projection Settings")]
-    public bool drawProjectedAOnB;
-    public bool drawProjectedBOnA;
+    public ProjectionMode vectorToProject;
 
     // Draw Projected vectors for rule A.B = |A||projB(onA)| = |B||projA(onB)|
 
     protected override Vector3 PerformOperation(Vector3 a, Vector3 b)
     {
 
+        switch (vectorToProject)
+        {
+            case ProjectionMode.drawProjectedAOnB:
+                return Vector3.Project(a, b);
+
+            case ProjectionMode.drawProjectedBOnA:
+                return Vector3.Project(b, a);
+        }
+
+
         return Vector3.zero;
     }
 
+    protected override string GetOperationName()
+    {
+        return vectorToProject == ProjectionMode.drawProjectedAOnB
+            ? "proj|A| on B"
+            : "proj|B| on A";
+    }
 
- }
+}
