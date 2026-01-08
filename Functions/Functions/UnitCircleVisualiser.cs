53adccf3e6710724add6d669813d97b7fe7717be
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-08 22:38:38 +0000

    Added Unit Circle Visualiser

--- Functions/Functions/UnitCircleVisualiser.cs
+++ Functions/Functions/UnitCircleVisualiser.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class UnitCircleVisualiser : VisualiserBase
 {
     [Header("Connected Graph")]
-    [Tooltip("Drag your Sine Wave game object here")]
     public FunctionVisualiserBase connectedGraph;
 
     [Header("Settings")]
