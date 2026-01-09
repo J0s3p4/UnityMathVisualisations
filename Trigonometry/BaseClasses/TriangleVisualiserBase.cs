0b0d8d05b2ee83ba0149c621805913430dbb1a4e
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-09 12:55:01 +0000

    Updated TriangleVisBase and Triangle SAS Vis
    - Triangle Vis base can draw a triangle
    - Can now solve side angle side
    - Sides are also labelled A,B,C

--- Trigonometry/BaseClasses/TriangleVisualiserBase.cs
+++ Trigonometry/BaseClasses/TriangleVisualiserBase.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public abstract class TriangleVisualiserBase : VisualiserBase
@@ -7,15 +8,38 @@ public abstract class TriangleVisualiserBase : VisualiserBase
     public Color triangleColor = Color.yellow;
     // Label sides?
 
+    protected Vector3 p1, p2, p3;
+    
 
     protected override void Draw()
     {
         CalculateVertices();
 
         //Draw Lines
-        // Line A a -> b
-        // Line B b -> c
-        // Line C c -> d
+        Gizmos.color = triangleColor;
+        Gizmos.DrawLine(p1, p2); //A
+        Gizmos.DrawLine(p2, p3); //B
+        Gizmos.DrawLine(p3, p1); //C
+
+        # if UNITY_EDITOR
+        // Label Lines
+        Vector3 midpointA = (p1 + p2) * 0.5f;
+        Vector3 midpointB = (p2 + p3) * 0.5f;
+        Vector3 midpointC = (p3 + p1) * 0.5f;
+        Handles.Label(
+            midpointA,
+            $"A"
+        );
+        Handles.Label(
+           midpointB,
+           $"B"
+        );
+        Handles.Label(
+            midpointC,
+           $"C"
+        );
+        #endif
+
 
         //Draw Angles
 
--- Trigonometry/Triangles/TriangleSASVisualiser.cs
+++ Trigonometry/Triangles/TriangleSASVisualiser.cs
@@ -1,10 +1,34 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class TriangleSASVisualiser : TriangleVisualiserBase
 {
 
+    public float sideA;
+    public float sideB;
+    [Range(0, 360)] public float angle;
+
+    // p1>p2 side A, p2>p3 side B, p3>p1 side C
+
     protected override void CalculateVertices()
     {
+        // side A and B meet at origin, this is where the angle is
+        p1 = Vector3.zero;
+
+        p2 = new Vector3(sideA, 0, 0); // Draw side A along x-axis
+
+        // v3 is the end of Side B, calculated using the angle
+
+        // ToDo: take radians or degrees
+        float rad = angle * Mathf.Deg2Rad; // Convert degrees to radians
+
+        // Polar to Cartesian conversion
+        p3 = new Vector3(
+            Mathf.Cos(rad) * sideB,
+            Mathf.Sin(rad) * sideB,
+            0
+        );
+
         return;
     }
 }
