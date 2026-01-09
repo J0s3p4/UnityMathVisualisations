e9ec8f32aa1aa9eaa4b09846c8e893f95d7f3c7f
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-09 11:46:28 +0000

    Created Triangle Visualiser Base

---
+++ Trigonometry/BaseClasses/TriangleVisualiserBase.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public abstract class TriangleVisualiserBase : VisualiserBase
+{
+
+    [Header("Triangle Settings")]
+    public Color triangleColor = Color.yellow;
+    // Label sides?
+
+
+    protected override void Draw()
+    {
+        CalculateVertices();
+
+        //Draw Lines
+        // Line A a -> b
+        // Line B b -> c
+        // Line C c -> d
+
+    }
+
+    // Each solver will calculate vertices
+    protected abstract void CalculateVertices();
+
+}
