30764014d02b7501351b4f909da8b03422527bbd
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-12 21:22:12 +0000

    Added function to ASA vis script

--- Trigonometry/Triangles/Triangle ASA Visualiser.cs
+++ Trigonometry/Triangles/Triangle ASA Visualiser.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 
-public class TriangleASAVisualiser : MonoBehaviour
+public class TriangleASAVisualiser : TriangleVisualiserBase
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("ASA Settings")]
+    public float sideC;              // p1 -> p2
+    [Range(0, 179)] public float angleAB; // at p1
+    [Range(0, 179)] public float angleAC; // at p2
 
-    // Update is called once per frame
-    void Update()
+    protected override void CalculateVertices()
     {
-        
+        // Validate angles
+        if (angleAB + angleAC >= 180f)
+        {
+            p1 = p2 = p3 = Vector3.zero;
+            return;
+        }
+
+        // Convert to radians
+        float radA = angleAB * Mathf.Deg2Rad;
+        float radB = angleAC * Mathf.Deg2Rad;
+        float radC = Mathf.PI - radA - radB;
+
+        // Law of Sines
+        float sideA = sideC * Mathf.Sin(radA) / Mathf.Sin(radC);
+        float sideB = sideC * Mathf.Sin(radB) / Mathf.Sin(radC);
+
+        // Place base
+        p1 = Vector3.zero;
+        p2 = new Vector3(sideC, 0f, 0f);
+
+        // Build p3 from p1
+        p3 = new Vector3(
+            Mathf.Cos(radA) * sideB,
+            Mathf.Sin(radA) * sideB,
+            0f
+        );
     }
 }
