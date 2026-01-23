3362ff063eabde3db65f7f0c010e97a4167e4411
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-23 22:29:16 +0000

    Points plotting

--- Graphs/BaseClasses/PlotGraphBase.cs
+++ Graphs/BaseClasses/PlotGraphBase.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEditor;
 
 public class PlotGraphBase : VisualiserBase
 {
@@ -6,19 +7,21 @@ public class PlotGraphBase : VisualiserBase
     public Vector3[] pointArray;
 
     public float pointSphereRadius = 0.1f;
+    public Color displayColour = Color.red;
+
 
     protected override void Draw()
     {
-        if (pointArray != null) // No values return
+        if (pointArray != null) // If point array populated
         {
-            return;
-        }
-
-        for (int i = 0; i < pointArray.Length; i++) // Plot graph
-        {
-            Gizmos.DrawSphere(pointArray[i], pointSphereRadius);
+            for (int i = 0; i < pointArray.Length; i++) // Plot graph
+            {
+                Gizmos.color = displayColour;
+                Gizmos.DrawSphere(pointArray[i], pointSphereRadius);
+            }
         }
 
     }
+
 
 }
