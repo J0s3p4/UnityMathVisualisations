6fe494af21c0c9a94862552ed1ef09eac86e3975
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2026-01-23 22:13:32 +0000

    Set up draw point functionality

--- Graphs/BaseClasses/PlotGraphBase.cs
+++ Graphs/BaseClasses/PlotGraphBase.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class PlotGraphBase : VisualiserBase
 {
 
+    private Vector3[] pointArray;
+
+    public float pointSphereRadius;
 
     protected override void Draw()
     {
+        for (int i = 0; i < pointArray.Length; i++)
+        {
+            Gizmos.DrawSphere(pointArray[i], pointSphereRadius);
+        }
+
     }
 
 }
